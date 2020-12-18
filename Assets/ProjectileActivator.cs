aef31c0f620ac9e4e723d9204469239bb5f5dbdd
Kris Krej <kris@krej.net>
2020-12-18 14:50:15 +0100

    Platformer

---
+++ Assets/ProjectileActivator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class ProjectileActivator : MonoBehaviour {
+    [SerializeField] GameObject objectToActivate;
+
+    void Awake() {
+        objectToActivate.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        objectToActivate.SetActive(true);
+    }
+}
---
+++ Assets/RotateInTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RotateInTime : MonoBehaviour {
+    void Awake() {
+        transform.Rotate(-new Vector3(0, 0, 1), Random.Range(0,360));
+    }
+
+    void Update() {
+        transform.Rotate(-new Vector3(0, 0, 1), Time.deltaTime*180);
+    }
+}
